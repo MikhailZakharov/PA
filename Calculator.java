7e60f5c96275944dbc1b2483e878197b6d475631
Захаров Михаил Владимирович <m.zakharov@qiwi.com>
2016-03-24 15:14:09 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+
+/*
+Класс реализует калькулятор
+*/
+public class Calculator{
+	
+	private int result; //результат вычисления
+	
+	public void add(int ... params){
+		for(Integer param : params){
+			this.result += param;
+		}
+	}
+
+	public int getResult(){
+		return this.result;
+	}
+
+	public void cleanResult(){
+		this.result = 0;
+	}
+}	
